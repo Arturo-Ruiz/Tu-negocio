c3144456865d3c94704260b42690caf278d25363
Arturo <gregory09ruiz@gmail.com>
2020-12-29 13:36:27 -0400

    migrations added

--- database/migrations/2020_12_29_172625_create_roles_table.php
+++ database/migrations/2020_12_29_172625_create_roles_table.php
@@ -4,7 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-class CreateProductsTable extends Migration
+class CreateRolesTable extends Migration
 {
     /**
      * Run the migrations.
@@ -13,16 +13,10 @@ class CreateProductsTable extends Migration
      */
     public function up()
     {
-        Schema::create('products', function (Blueprint $table) {
+        Schema::create('roles', function (Blueprint $table) {
             $table->id();
             $table->string('name');
-            $table->mediumText('description');
-            $table->string('price');
-            $table->string('img');
-            $table->unsignedBigInteger('category_id')->unsigned();
             $table->timestamps();
-
-            $table->foreign('category_id')->references('id')->on('categories');
         });
     }
 
@@ -33,6 +27,6 @@ class CreateProductsTable extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('products');
+        Schema::dropIfExists('roles');
     }
 }
