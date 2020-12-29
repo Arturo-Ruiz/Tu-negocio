2dc8ddad47fde2052d7a76c475d64190dd45087d
Arturo <gregory09ruiz@gmail.com>
2020-12-29 13:39:40 -0400

    seeders

---
+++ database/seeds/UsersTableSeeder.php
@@ -0,0 +1,21 @@
+<?php
+
+use Illuminate\Database\Seeder;
+
+class UsersTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        App\User::create([
+            'name' => 'Arturo Ruiz',
+            'email' => 'gregory09ruiz@gmail.com',
+            'password' => bcrypt('123'),
+            'role' => 'Administrador',
+        ]);
+    }
+}
